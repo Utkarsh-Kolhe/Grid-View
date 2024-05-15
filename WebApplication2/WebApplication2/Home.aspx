de6de229c8afc6baac15c78f277d8b664e8f4b20
67104_utkarsh <utkarshkolhe9911@gmail.com>
2024-05-16 00:37:08 +0530

    Grid View

---
+++ WebApplication2/WebApplication2/Home.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="WebApplication2.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 117px;
+        }
+        .auto-style3 {
+            width: 116px;
+        }
+        .auto-style4 {
+            width: 89px;
+        }
+        .auto-style5 {
+            width: 423px;
+        }
+        .auto-style6 {
+            width: 247px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td>
+                        <table class="auto-style1">
+                            <tr>
+                                <td class="auto-style2">
+                                    <asp:Label ID="Label1" runat="server" Text="Investigation"></asp:Label>
+                                </td>
+                                <td class="auto-style3">
+                                    <asp:DropDownList ID="InvestigationDropDownList" runat="server">
+                                        <asp:ListItem Value="-1">Select</asp:ListItem>
+                                        <asp:ListItem Value="1">Utkarsh</asp:ListItem>
+                                        <asp:ListItem Value="2">Kaustubh</asp:ListItem>
+                                        <asp:ListItem Value="3">Lokesh</asp:ListItem>
+                                        <asp:ListItem Value="4">Somesh</asp:ListItem>
+                                    </asp:DropDownList>
+                                </td>
+                                <td class="auto-style4">
+                                    <asp:Label ID="Label2" runat="server" Text="Marks"></asp:Label>
+                                </td>
+                                <td>
+                                    <asp:TextBox ID="MarksTextBox" runat="server"></asp:TextBox>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table class="auto-style1">
+                            <tr>
+                                <td class="auto-style5">&nbsp;</td>
+                                <td>
+                                    <asp:Button ID="AddToGridButton" runat="server" OnClick="AddToGridButton_Click" Text="Add To Grid" />
+                                </td>
+                                <td>&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table class="auto-style1">
+                            <tr>
+                                <td class="auto-style6">&nbsp;</td>
+                                <td>
+                                    <%--<asp:GridView ID="GridView1" runat="server">
+                                    </asp:GridView>--%>
+                                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowDataBound="GridView1_RowDataBound">
+                                        <Columns>
+                                            <asp:BoundField DataField="S_No" HeaderText="S. No" />
+                                            <asp:BoundField DataField="Investigation" HeaderText="Investigation" />
+                                            <asp:BoundField DataField="Marks" HeaderText="Marks" />
+                                            <asp:TemplateField HeaderText="Actions">
+                                                <%--<ItemTemplate>
+                                                    <asp:Button runat="server" CommandName="Edit" Text="Edit" CssClass="btn btn-primary" />
+                                                    <asp:Button runat="server" CommandName="Delete" Text="Delete" CssClass="btn btn-danger" />
+                                                </ItemTemplate>--%>
+                                            </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+                                </td>
+                                <td>&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebApplication2/Home.aspx.cs
@@ -0,0 +1,150 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//using System.Web;
+//using System.Web.UI;
+//using System.Web.UI.WebControls;
+
+//namespace WebApplication2
+//{
+//    class Demo
+//    {
+//        public string S_No { get; set; }
+//        public string Investigation {  get; set; }
+//        public string Marks { get; set;}
+//    }
+//    public partial class Home : System.Web.UI.Page
+//    {
+//        static List<Demo> lst = new List<Demo>();
+//        static int count = 0;
+//        protected void Page_Load(object sender, EventArgs e)
+//        {
+//            GrideViewTable();
+//        }
+
+//        protected void AddToGridButton_Click(object sender, EventArgs e)
+//        {
+//            Demo demo = new Demo();
+//            demo.S_No = (++count).ToString();
+//            demo.Investigation = InvestigationDropDownList.Text;
+//            demo.Marks = MarksTextBox.Text;
+
+//            lst.Add(demo);
+
+//            GrideViewTable();
+//        }
+
+//        public void GrideViewTable()
+//        {
+//            GridView1.DataSource = lst;
+//            GridView1.DataBind();
+//        }
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    class Demo
+    {
+        public string S_No { get; set; }
+        public string Investigation { get; set; }
+        public string Marks { get; set; }
+    }
+    public partial class Home : System.Web.UI.Page
+    {
+        static List<Demo> lst = new List<Demo>();
+        static int count = 0;
+        static int editIndex = -1; // Track the index of the row being edited
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GrideViewTable();
+            }
+        }
+
+        protected void AddToGridButton_Click(object sender, EventArgs e)
+        {
+            Demo demo = new Demo();
+            if (editIndex != -1) // Check if editing
+            {
+                demo = lst[editIndex];
+                demo.Investigation = InvestigationDropDownList.Text;
+                demo.Marks = MarksTextBox.Text;
+                lst[editIndex] = demo;
+                editIndex = -1; // Reset edit index
+            }
+            else
+            {
+                demo.S_No = (++count).ToString();
+                demo.Investigation = InvestigationDropDownList.Text;
+                demo.Marks = MarksTextBox.Text;
+                lst.Add(demo);
+            }
+            GrideViewTable();
+            ClearFields();
+        }
+
+        protected void ClearFields()
+        {
+            InvestigationDropDownList.SelectedIndex = 0;
+            MarksTextBox.Text = "";
+        }
+
+        public void GrideViewTable()
+        {
+            GridView1.DataSource = lst;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                // Create the Edit button
+                Button btnEdit = new Button();
+                btnEdit.CommandName = "Edit";
+                btnEdit.Text = "Edit";
+                btnEdit.CssClass = "btn btn-primary";
+                btnEdit.CommandArgument = e.Row.RowIndex.ToString();
+                btnEdit.Click += new EventHandler(btnEdit_Click);
+
+                // Create the Delete button
+                Button btnDelete = new Button();
+                btnDelete.CommandName = "Delete";
+                btnDelete.Text = "Delete";
+                btnDelete.CssClass = "btn btn-danger";
+                btnDelete.CommandArgument = e.Row.RowIndex.ToString();
+                btnDelete.Click += new EventHandler(btnDelete_Click);
+
+                // Add buttons to the GridView
+                e.Row.Cells[3].Controls.Add(btnEdit); // Assuming the edit column is at index 3
+                e.Row.Cells[3].Controls.Add(btnDelete); // Assuming the delete column is at index 3
+            }
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            editIndex = int.Parse(btn.CommandArgument);
+            Demo demo = lst[editIndex];
+            InvestigationDropDownList.Text = demo.Investigation;
+            MarksTextBox.Text = demo.Marks;
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int rowIndex = int.Parse(btn.CommandArgument);
+            lst.RemoveAt(rowIndex);
+            GrideViewTable();
+        }
+    }
+}
